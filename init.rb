48fa14f5fec2ce8a4ab3132a058bba2399565840
unknown <D:\!!!!\mail>
2012-06-04 11:59:02 +0400

    initial commit

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_additional_history do
+  name 'Redmine Additional History plugin'
+  author 'Alexey Romanov'
+  description 'Add comments to ticket on some actions'
+  version '0.0.1'
+  url 'http://logiceditor.com'
+  author_url 'http://logiceditor.com'
+end
